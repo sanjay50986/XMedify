2bf5edf83348d5984d8c01bb3f69e9ed0ddc538e
Sanjay Jangid <sanjay50986@gmail.com>
2025-12-26 00:34:01 +0530

    add image

--- src/components/Hostpital/HospitalCard.jsx
+++ src/components/Hostpital/HospitalCard.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import HospitalImage from '../../assets/hospital.png'
+import HospitalImage from '../../assets/Hospital.png'
 
 const HospitalCard = ({ hospital }) => {
 
     console.log(hospital)
     return (
-        <div className='hospital-card d-flex flex-column flex-md-row justify-content-between'>
+        <div className='hospital-card d-flex flex-column flex-md-row justify-content-between mt-3'>
             <img className='d-none d-md-block' width={150} height={150} src={HospitalImage} alt="hospital" />
             <div className='hospital-info mt-0 mt-md-5'>
                 <h2>{hospital['Hospital Name']}</h2>

0426388c5fa1dcefe98d18af31020cd1145a6293
Sanjay Jangid <sanjay50986@gmail.com>
2025-12-27 22:50:30 +0530

    fix booking issue

--- src/pages/MyBooking.jsx
+++ src/pages/MyBooking.jsx
@@ -24,13 +24,15 @@ const MyBooking = () => {
 
                 <div className='my-bookings'>
                     <div className='container py-5'>
-                       {bookingData.map((booking, index) => (
+                       {!bookingData ? (
+                        <h2>No Bookings Found</h2>
+                       ) : (bookingData.map((booking, index) => (
                         <div className='hospital-card mt-3' key={index}>
                             <div className='d-flex flex-column flex-md-row justify-content-between'>
                                  <img className='d-none d-md-block' width={150} height={150} src={HospitalImage} alt="hospital" />
                                  <div className='hospital-info mt-0 mt-md-5'>
-                                    <h3 style={{textAlign: "left", color: "#14BEF0"}}>{booking.hospitalName}</h3>
-                                    <h4>{booking.address}</h4>
+                                    <h3 style={{textAlign: "left", color: "#14BEF0"}}>{booking?.hospitalName}</h3>
+                                    <h4>{booking?.address}</h4>
                                     <p className='types'>Smilessence Center for Advanced Dentistry + 1</p>
                                     <p className='fee'><span>FREE</span> Consultation fee at clinic</p>
                 
@@ -42,17 +44,17 @@ const MyBooking = () => {
 
                                 <div className='mt-5'>
                                     <span className='time-box'>
-                                        {booking.time}
+                                        {booking?.time}
                                     </span>
 
                                     <span className='date-box'>
-                                        {booking.date} 2026
+                                        {booking?.date} 2026
                                     </span>
                                 </div>
                             </div>
 
                         </div>
-                       ))}
+                       )))}
                     </div>
                 </div>
             </div>
